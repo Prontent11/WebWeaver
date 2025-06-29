73da2402c9a880bf807f2c743572435acf67c228
Yash <yashyadao2001@gmail.com>
2025-06-29 10:44:49 +0530

    prisma & trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+'use client'
 import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useState } from "react";
+
 
 const Page = () => {
+  const [count,setCount]=useState(0);
+  const [count1,setCount1]=useState(0);
+  console.log("rendering count...")
+  const handleSetCount=()=>{
+    setCount(count+1);
+    console.log("setcount rendering..."+count);
+    setCount1(count1+1);
+    console.log("setcount1 rendering..."+count1);
+  }
   return <div>
-    <Button >Yash</Button>
+    <Button onClick={handleSetCount} >+</Button>
+ 
   </div>;
 };
 
